865a5e55e6fc5628779c8412354b6fbf45f06829
Yuda Cogati <yudapc@gmail.com>
2018-11-09 17:08:24 +0700

    changed code array sum

--- problems/array-sum/array-sum.ts
+++ problems/array-sum/array-sum.ts
@@ -1,16 +1,11 @@
-function arraySum(arr):number {
-    let sum: number = 0;
-    let dArr: number[] = [...arr];
-    let dimension: number = 3;
-    let flatArr = dArr.reduce((acc, val) => acc.concat(val), []);
-  
-    for (let i = 0; i < dimension; i++) {
-      flatArr = flatArr.reduce((acc, val) => acc.concat(val), []);
-    }
-  
-    flatArr.forEach(ele => sum += ele);
-                    
-    return sum;
-  }
-  
-  console.log(arraySum([1,2,[3,4,[5]]]));
+const flatten = (arr: []) => {
+  return arr.reduce(
+    (flat, toFlatten) =>
+      flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten),
+    []
+  );
+};
+
+const arraySum = arr => flatten(arr).reduce((a, b) => a + b);
+
+console.log(arraySum([1, 2, [3, 4, [5]]]));
